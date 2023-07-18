8cadb8a284159c7a6b9a9ee5f050de1336afa75b
lilly <lilly1987@naver.com>
2023-07-18 17:35:29 +0900

    Update wildcards.py
    https://github.com/lilly1987/ComfyUI_node_Lilly/pull/8/commits/c91e62428f32171aaf10ed6e71138371a1854a19

--- wildcards.py
+++ wildcards.py
@@ -14,7 +14,7 @@ else:
 class wildcards:
 
     # 가져올 파일 목록
-    card_path=os.path.dirname(__file__)+"\\..\\..\\wildcards\\**\\*.txt"
+    card_path = os.path.join(os.path.dirname(__file__), "..", "..", "wildcards", "**", "*.txt")
     #card_path=f"{os.getcwd()}\\wildcards\\**\\*.txt"
     print(f"wildcards card_path : ", card_path , style="bold CYAN")
 
@@ -137,6 +137,7 @@ class wildcards:
         
         for file in files:
             basename = os.path.basename(file)
+            basename = os.path.relpath(file, os.path.dirname(__file__)).replace("\\", "/").replace("../../wildcards/", "")
             file_name = os.path.splitext(basename)[0]
             if not file_name in cards:
                 cards[file_name]=[]
